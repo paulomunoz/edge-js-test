f0afe5ba9dbe9550528a372141baf057edfc0fee
Paulo Munoz <pvanet@gmail.com>
2018-02-06 21:33:39 +0000

    Initial commit

---
+++ ConsoleApplication1/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using EdgeJs;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        public static async Task Start()
+        {
+            var func = Edge.Func(@"
+                return function (data, callback) {
+                    callback(null, 'Node.js welcomes ' + data);
+                }
+            ");
+
+            Console.WriteLine(await func(".NET"));
+        }
+
+        static void Main(string[] args)
+        {
+            Start().Wait();
+            Console.ReadLine();
+        }
+    }
+}
